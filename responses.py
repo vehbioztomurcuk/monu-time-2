fa4a931283581f6fbb1a29d2bb14e49240c085b1
vehbioztomurcuk <vehbi.oztomurcuk@gmail.com>
2023-07-06 09:43:46 +0300

    addtional commands

--- responses.py
+++ responses.py
@@ -44,8 +44,50 @@ def get_response(message: str) -> str:
         last_monu2_time = now
         return result
 
-    if p_message == '!yardim' or p_message == '!yardım':
+    if p_message == '!yardim' or p_message == '!yardım'or p_message == '!help':
         return '- Monument kesildikten hemen sonra **!monu** yazınız. Bot bir sonraki monu çıkış saatlerini 60 ve 90 dakika sonrası olarak bildirecektir.\n- Dv 2 monument kesildikten hemen sonra **!monu2** yazınız. Bot bir sonraki dv2 monu çıkış saatlerini 60 ve 90 dakika sonrası olarak bildirecektir.\n- Bot tarafından girilmiş olan son saatleri görmek için **!log** yazabilirsiniz.'
+
+    if p_message == '!rün'or p_message == '!rune':
+        return '>>> **Rise Online World (Erken Erişim) - Rünler**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-erken-erisim-runler.17973/'
+    
+    if p_message == '!craft'or p_message == '!meslek'or p_message == '!meslekler':
+        return '>>> **1. Rise Online World (Erken Erişim) - Craft Sistemi ve Meslekler**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-erken-erisim-craft-sistemi-ve-meslekler.13888/\n**2. Rise Online World - Meslekler Rehberi (Erken Erişim)**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-meslekler-rehberi-erken-erisim.12903/'
+    
+    if p_message == '!weaponcraft'or p_message == '!silahcraft':
+        return '>>> **Rise Online World (Erken Erişim) - Silah Üretimi (Weapon Smithing)**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-erken-erisim-silah-uretimi-weapon-smithing-guncellemesi.17984/'
+    
+    if p_message == '!jewelcraft'or p_message == '!takıcraft':
+        return '>>> **Rise Online World (Erken Erişim) - Takı Üretimi (Jewel Crafting)**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-erken-erisim-taki-uretimi-jewel-crafting-guncellemesi.17986/'
+
+    if p_message == '!clan'or p_message == '!klan':
+        return '>>> **Rise Online World (Erken Erişim) - Klan Binalarının Özellikleri**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-erken-erisim-klan-binalarinin-ozellikleri.17331/#post-123382'
+
+    if p_message == '!silah'or p_message == '!weapons':
+        return '>>> **Rise Online World - Silahlar**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-silah-duzenlemeleri.15711/'
+    
+    if p_message == '!takı'or p_message == '!accessories':
+        return '>>> **Takı Upgrade Sistemi ve Takılar:**\nhttps://forum.riseonlineworld.com/konular/taki-upgrade-sistemi-aktif-edildi.17972/'
+    
+    if p_message == '!belt'or p_message == '!kemer':
+        return '>>> **Oyundaki beltler:**\nhttps://forum.riseonlineworld.com/attachments/belts-jpg.13555/'
+    
+    if p_message == '!ring' or p_message == '!yüzük':
+        return '>>> **Oyundaki yüzükler:**\nhttps://forum.riseonlineworld.com/attachments/rings-jpg.13556/'
+    
+    if p_message == '!küpe'or p_message == '!earring':
+        return '>>> **Oyundaki küpeler:**\nhttps://forum.riseonlineworld.com/attachments/earrings-jpg.13557/'
+    
+    if p_message == '!neck'or p_message == '!kolye'or p_message == '!pendant':
+        return '>>> **Oyundaki kolyeler:**\nhttps://forum.riseonlineworld.com/attachments/necklaces-jpg.13558/'
+    
+    if p_message == '!hata'or p_message == '!error':
+        return '>>> **Tüm Hatalar ve Çözümleri**\nhttps://forum.riseonlineworld.com/konular/tum-hatalar-ve-cozumleri.6163/'
+    
+    if p_message == '!upgrade':
+        return '>>> **Upgrade oranları**\nhttps://forum.riseonlineworld.com/konular/rise-online-world-basarili-upgrade-oranlari.11847/'
+    
+
+    
 
     if p_message == '!log':
         if log:
